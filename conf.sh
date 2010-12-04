bb117ecc7313c7f87eff3e75ddc0afb8fd116a3a
admin <gadilif@gmail.com>
2010-12-04 17:58:31 +0200

    testing git commit via textmate

--- conf.sh
+++ conf.sh
@@ -6,3 +6,4 @@ export SCALA="$SCALA_HOME/bin/scala"
 export PATH="$JAVA_HOME/bin:$SCALA_HOME/bin:/usr/local/mysql/bin:$PATH"
 export MYSQL_CONNECTOR_JAR="/Users/admin/etherpad/lib/mysql-connector-java-5.1.13-bin.jar"
 export SCALA_LIBRARAY_JAR=/usr/local/scala/lib/scala-library.jar
+
